bbbe587959021f2058657fd2a0db4bc04c072cb8
Edwin <me@edwin.one>
2026-02-15 03:09:56 -0800

    Update utls import path to new repository

--- newreverse.go
+++ newreverse.go
@@ -22,7 +22,7 @@ import (
 	"sync/atomic"
 	"time"
 
-	utls "github.com/refraction-networking/utls"
+	utls "github.com/edthepurple/utls"
 )
 
 // ---------------------------------------------------------------------------
